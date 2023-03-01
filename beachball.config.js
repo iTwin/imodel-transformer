bdd66c4c67083b2f764a197b28b17c3b7ab89a38
Michael Belousov <mike.belousov@bentley.com>
2023-03-01 13:30:51 -0500

    copy over pipelines from presentation repo

---
+++ beachball.config.js
@@ -0,0 +1,34 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+// @ts-check
+/** @type {import("beachball").BeachballConfig } */
+module.exports = {
+  bumpDeps: false,
+  access: "public",
+  tag: "latest",
+  scope: [
+    "packages/**"
+  ],
+  ignorePatterns: [
+    ".nycrc",
+    ".eslintrc.json",
+    ".mocharc.json",
+    "tsconfig.*",
+    ".*ignore",
+    ".github/**",
+    ".vscode/**",
+    "**/test/**",
+    "pnpm-lock.yaml",
+  ],
+  changehint: "Run 'pnpm change' to generate a change file",
+  changelog: {
+    customRenderers: {
+      renderEntry: (entry) => {
+        const commitLink = `https://github.com/iTwin/transformer/commit/${entry.commit}`;
+        return `- ${entry.comment} ([commit](${commitLink}))`;
+      },
+    },
+  },
+};
