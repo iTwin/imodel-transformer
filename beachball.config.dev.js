65a04de913e71ccbf9db6e6f6798648a76bd225e
Michael Belousov <mike.belousov@bentley.com>
2023-03-01 14:12:58 -0500

    Pipelines, CODEOWNERS, and cleanup (#2)
    * fix references to itwinjs-core and add some copied gh workflows
    
    * move .github to repo root, oops
    
    * copy over pipelines from presentation repo
    
    * fix master branch refs
    
    * remove unneeded gh workflows
    
    * fix copyright linter

---
+++ beachball.config.dev.js
@@ -0,0 +1,15 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+// @ts-check
+const base = require("./beachball.config.js");
+
+/** @type {import("beachball").BeachballConfig } */
+module.exports = {
+  ...base,
+  tag: "nightly",
+  prereleasePrefix: "dev",
+  generateChangelog: false,
+  gitTags: false,
+};
