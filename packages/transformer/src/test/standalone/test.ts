0ae37aa2df799bbab201df7d48055bd1c6fcc84e
Michael Belousov <mike.belousov@bentley.com>
2023-03-22 22:26:05 -0400

    prune imports in test file

--- packages/transformer/src/test/standalone/test.ts
+++ packages/transformer/src/test/standalone/test.ts
@@ -3,39 +3,11 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-import { assert, expect } from "chai";
-import * as fs from "fs";
-import * as path from "path";
-import * as Semver from "semver";
-import * as sinon from "sinon";
-import {
-  CategorySelector, DisplayStyle3d, DocumentListModel, Drawing, DrawingCategory, DrawingGraphic, DrawingModel, ECSqlStatement, Element,
-  ElementMultiAspect, ElementOwnsChildElements, ElementOwnsExternalSourceAspects, ElementOwnsMultiAspects, ElementOwnsUniqueAspect, ElementRefersToElements,
-  ElementUniqueAspect, ExternalSourceAspect, GenericPhysicalMaterial, GeometricElement, IModelDb, IModelElementCloneContext, IModelHost, IModelJsFs,
-  InformationRecordModel, InformationRecordPartition, LinkElement, Model, ModelSelector, OrthographicViewDefinition,
-  PhysicalModel, PhysicalObject, PhysicalPartition, PhysicalType, Relationship, RenderMaterialElement, RepositoryLink, Schema, SnapshotDb, SpatialCategory, StandaloneDb,
-  SubCategory, Subject, Texture
-} from "@itwin/core-backend";
-//import * as ECSchemaMetaData from "@itwin/ecschema-metadata";
-import * as BackendTestUtils from "@itwin/core-backend/lib/cjs/test";
-import { DbResult, Guid, Id64, Id64String, Logger, LogLevel, OpenMode } from "@itwin/core-bentley";
-import {
-  AxisAlignedBox3d, BriefcaseIdValue, Code, CodeScopeSpec, CodeSpec, ColorDef, CreateIModelProps, DefinitionElementProps, ElementAspectProps, ElementProps,
-  ExternalSourceAspectProps, ImageSourceFormat, IModel, IModelError, PhysicalElementProps, Placement3d, ProfileOptions, QueryRowFormat, RelatedElement, RelationshipProps,
-} from "@itwin/core-common";
-//import { Point3d, Range3d, StandardViewIndex, Transform, YawPitchRollAngles } from "@itwin/core-geometry";
-import { IModelExporter, IModelExportHandler, IModelTransformer, IModelTransformOptions, TransformerLoggerCategory } from "../../core-transformer";
-import {
-  AspectTrackingImporter,
-  AspectTrackingTransformer,
-  assertIdentityTransformation, AssertOrderTransformer,
-  ClassCounter, FilterByViewTransformer, IModelToTextFileExporter, IModelTransformer3d, IModelTransformerTestUtils, PhysicalModelConsolidator,
-  RecordingIModelImporter, runWithCpuProfiler, TestIModelTransformer, TransformerExtensiveTestScenario,
-} from "../IModelTransformerUtils";
-import { KnownTestLocations } from "../KnownTestLocations";
-
-//import "./TransformerTestStartup"; // calls startup/shutdown IModelHost before/after all tests
-import { SchemaLoader } from "@itwin/ecschema-metadata";
+import { expect } from "chai";
+import { IModelHost, SnapshotDb } from "@itwin/core-backend";
+import { Logger, LogLevel } from "@itwin/core-bentley";
+import { IModelTransformer, TransformerLoggerCategory } from "../../transformer";
+import { assertIdentityTransformation, IModelTransformerTestUtils } from "../IModelTransformerUtils";
 
 async function main() {
   await IModelHost.startup();
