fd2161f8e43007a5bcdc04d8b0d241d96f2da1c3
Michael Belousov <mike.belousov@bentley.com>
2023-03-22 23:18:25 -0400

    fix where mkdir occurs

--- packages/performance-scripts/multi-branch-test.sh
+++ packages/performance-scripts/multi-branch-test.sh
@@ -3,22 +3,24 @@
 BRANCHES="${@:-performance-scripts experiment-fast-transformer linear-traversal}"
 
 # cd $0/../test-app
+pushd ../test-app
 
 for branch in $BRANCHES; do
   git checkout $branch
   pnpm i
   pnpm -r clean
   pnpm -r build
+
   DIR="result_$branch"
   mkdir $DIR
 
-  pushd ../test-app
   for prof_type in sqlite js-cpu linux-native; do
     PROFILE_TYPE=$prof_type NODE_OPTIONS='-r ../performance-scripts' \
       node lib/Main.js --sourceFile ~/work/bad-aspect-old.bim \
           --targetDestination /tmp/out.bim
     mv *.cpuprofile *.sqliteprofile.db $DIR
   done
-  popd
 done
+
+popd
 
