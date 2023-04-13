226015639e368752937fff570dcaf7616f909f8c
DanRod1999 <DanRod1999@users.noreply.github.com>
2023-04-13 12:38:15 -0400

    log username

--- packages/performance-tests/test/identity.test.ts
+++ packages/performance-tests/test/identity.test.ts
@@ -37,7 +37,7 @@ describe("imodel-transformer", () => {
       Logger.setLevel(TransformerLoggerCategory.IModelImporter, logLevel);
       Logger.setLevel(TransformerLoggerCategory.IModelTransformer, logLevel);
     }
-
+    console.log(process.env.V1_CHECKPOINT_USER_NAME)
     assert(process.env.V1_CHECKPOINT_USER_NAME, "user name was not configured");
     assert(process.env.V1_CHECKPOINT_USER_PASSWORD, "user password was not configured");
 
