24fe1bf2d1651459bb96b17fb3dec1d60e46460a
Daniel Rodriguez <DanRod1999@users.noreply.github.com>
2023-04-28 15:39:08 -0400

    wip

--- packages/performance-tests/test/identity.test.ts
+++ packages/performance-tests/test/identity.test.ts
@@ -41,12 +41,12 @@ describe("imodel-transformer", () => {
     }
     var usrEmail;
     var usrPass;
-    if(process.env.V2_CHECKPOINT_USER_NAME !== "undefined"){
+    if(process.env.V2_CHECKPOINT_USER_NAME != "undefined"){
       console.log('v2 check');
       usrEmail = process.env.V2_CHECKPOINT_USER_NAME;
       usrPass = process.env.V2_CHECKPOINT_USER_PASSWORD;
     }
-    else if(process.env.V1_CHECKPOINT_USER_NAME !== "undefined"){
+    else if(process.env.V1_CHECKPOINT_USER_NAME != "undefined"){
       console.log('v1 check');
       usrEmail = process.env.V1_CHECKPOINT_USER_NAME;
       usrPass = process.env.V1_CHECKPOINT_USER_PASSWORD;
@@ -56,6 +56,7 @@ describe("imodel-transformer", () => {
       usrEmail = process.env.IMODEL_USER_NAME;
       usrPass = process.env.IMODEL_USER_PASSWORD;
     }
+
     assert(usrEmail, "user name was not configured");
     assert(usrPass, "user password was not configured");
     const user = {
