40e7afedec11082f9abd2f37e91a6b39596eede5
Daniel Rodriguez <DanRod1999@users.noreply.github.com>
2023-05-03 08:41:15 -0400

    try skipping imodel

--- packages/performance-tests/test/identity.test.ts
+++ packages/performance-tests/test/identity.test.ts
@@ -97,64 +97,66 @@ describe("imodel-transformer", () => {
       //if (iModel.tShirtSize !== "m") continue;
       Logger.logInfo(loggerCategory, `processing iModel '${iModel.name}' of size '${iModel.tShirtSize.toUpperCase()}'`);
       console.log(iModel.name)
-      const sourceDb = await iModel.load();
-      const toGb = (bytes: number) => `${(bytes / 1024 **3).toFixed(2)}Gb`;
-      const sizeInGb = toGb(fs.statSync(sourceDb.pathName).size);
-      Logger.logInfo(loggerCategory, `loaded (${sizeInGb})'`);
-      const targetPath = initOutputFile(`${iModel.iTwinId}-${iModel.name}-target.bim`);
-      const targetDb = SnapshotDb.createEmpty(targetPath, {rootSubject: {name: iModel.name}});
-      class ProgressTransformer extends IModelTransformer {
-        private _count = 0;
-        private _increment() {
-          this._count++;
-          if (this._count % 1000 === 0) Logger.logInfo(loggerCategory, `exported ${this._count} entities`);
+      if(iModel.name != 'M - Hatch - PTMMR Secure'){
+        const sourceDb = await iModel.load();
+        const toGb = (bytes: number) => `${(bytes / 1024 **3).toFixed(2)}Gb`;
+        const sizeInGb = toGb(fs.statSync(sourceDb.pathName).size);
+        Logger.logInfo(loggerCategory, `loaded (${sizeInGb})'`);
+        const targetPath = initOutputFile(`${iModel.iTwinId}-${iModel.name}-target.bim`);
+        const targetDb = SnapshotDb.createEmpty(targetPath, {rootSubject: {name: iModel.name}});
+        class ProgressTransformer extends IModelTransformer {
+          private _count = 0;
+          private _increment() {
+            this._count++;
+            if (this._count % 1000 === 0) Logger.logInfo(loggerCategory, `exported ${this._count} entities`);
+          }
+          public override onExportElement(sourceElement: Element): void {
+            this._increment();
+            return super.onExportElement(sourceElement);
+          }
+          public override onExportRelationship(sourceRelationship: Relationship): void {
+            this._increment();
+            return super.onExportRelationship(sourceRelationship);
+          }
         }
-        public override onExportElement(sourceElement: Element): void {
-          this._increment();
-          return super.onExportElement(sourceElement);
+        const transformer = new ProgressTransformer(sourceDb, targetDb);
+        let schemaProcessingTimer: StopWatch | undefined;
+        let entityProcessingTimer: StopWatch | undefined;
+        try {
+          [schemaProcessingTimer] = await timed(async () => {
+            await transformer.processSchemas();
+          });
+          Logger.logInfo(loggerCategory, `schema processing time: ${schemaProcessingTimer.elapsedSeconds}`);
+          //[entityProcessingTimer] = await timed(async () => {
+          //await transformer.processAll();
+          //});
+          //Logger.logInfo(loggerCategory, `entity processing time: ${entityProcessingTimer.elapsedSeconds}`);
+        } catch (err: any) {
+          Logger.logInfo(loggerCategory, `An error was encountered: ${err.message}`);
+          const schemaDumpDir = fs.mkdtempSync("/tmp/identity-test-schemas-dump");
+          sourceDb.nativeDb.exportSchemas(schemaDumpDir);
+          Logger.logInfo(loggerCategory, `dumped schemas to: ${schemaDumpDir}`);
+        } finally {
+          const os = require('os');
+          const record = {
+            /* eslint-disable @typescript-eslint/naming-convention */
+            "Name": iModel.name,
+            "Id": iModel.iModelId,
+            "T-shirt size": iModel.tShirtSize,
+            "Size (Gb)": sizeInGb,
+            "Schema processing time": schemaProcessingTimer?.elapsedSeconds ?? "N/A",
+            "Entity processing time": entityProcessingTimer?.elapsedSeconds ?? "N/A",
+            /* eslint-enable @typescript-eslint/naming-convention */
+          };
+          report.push(record);
+          targetDb.close();
+          sourceDb.close();
+          transformer.dispose();
+          fs.appendFileSync(path.join(os.tmpdir(), "report.jsonl"), `${JSON.stringify(record)}\n`);
+          console.log("end of finally")
         }
-        public override onExportRelationship(sourceRelationship: Relationship): void {
-          this._increment();
-          return super.onExportRelationship(sourceRelationship);
-        }
+        console.log("end of for loop")
       }
-      const transformer = new ProgressTransformer(sourceDb, targetDb);
-      let schemaProcessingTimer: StopWatch | undefined;
-      let entityProcessingTimer: StopWatch | undefined;
-      try {
-        [schemaProcessingTimer] = await timed(async () => {
-          await transformer.processSchemas();
-        });
-        Logger.logInfo(loggerCategory, `schema processing time: ${schemaProcessingTimer.elapsedSeconds}`);
-        //[entityProcessingTimer] = await timed(async () => {
-        //await transformer.processAll();
-        //});
-        //Logger.logInfo(loggerCategory, `entity processing time: ${entityProcessingTimer.elapsedSeconds}`);
-      } catch (err: any) {
-        Logger.logInfo(loggerCategory, `An error was encountered: ${err.message}`);
-        const schemaDumpDir = fs.mkdtempSync("/tmp/identity-test-schemas-dump");
-        sourceDb.nativeDb.exportSchemas(schemaDumpDir);
-        Logger.logInfo(loggerCategory, `dumped schemas to: ${schemaDumpDir}`);
-      } finally {
-        const os = require('os');
-        const record = {
-          /* eslint-disable @typescript-eslint/naming-convention */
-          "Name": iModel.name,
-          "Id": iModel.iModelId,
-          "T-shirt size": iModel.tShirtSize,
-          "Size (Gb)": sizeInGb,
-          "Schema processing time": schemaProcessingTimer?.elapsedSeconds ?? "N/A",
-          "Entity processing time": entityProcessingTimer?.elapsedSeconds ?? "N/A",
-          /* eslint-enable @typescript-eslint/naming-convention */
-        };
-        report.push(record);
-        targetDb.close();
-        sourceDb.close();
-        transformer.dispose();
-        fs.appendFileSync(path.join(os.tmpdir(), "report.jsonl"), `${JSON.stringify(record)}\n`);
-        console.log("end of finally")
-      }
-      console.log("end of for loop")
     }
     console.log("exited for loop")
   });
