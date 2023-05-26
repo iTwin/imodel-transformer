9a0a893e59eae798296b532b0a4c486a0688fc5c
nick.tessier <22119573+nick4598@users.noreply.github.com>
2023-05-26 16:14:27 -0400

    use --delay for mocha to let us load our imodels in before we create tests

--- packages/performance-tests/test/TransformerRegression.test.ts
+++ packages/performance-tests/test/TransformerRegression.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /*---------------------------------------------------------------------------------------------
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
@@ -15,7 +16,7 @@ import * as fs from "fs";
 import { Element, IModelHost, IModelHostConfiguration, Relationship, SnapshotDb } from "@itwin/core-backend";
 import { Logger, LogLevel, PromiseReturnType, StopWatch } from "@itwin/core-bentley";
 import { IModelTransformer, TransformerLoggerCategory } from "@itwin/imodel-transformer";
-//import { TestBrowserAuthorizationClient } from "@itwin/oidc-signin-tool";
+// import { TestBrowserAuthorizationClient } from "@itwin/oidc-signin-tool";
 import { getTestIModels } from "./TestContext";
 import { initOutputFile } from "./TestUtils";
 import { NodeCliAuthorizationClient } from "@itwin/node-cli-authorization";
@@ -23,99 +24,165 @@ import { BackendIModelsAccess } from "@itwin/imodels-access-backend";
 import { IModelsClient } from "@itwin/imodels-client-authoring";
 import { TestBrowserAuthorizationClient } from "@itwin/oidc-signin-tool";
 import { Reporter } from "@itwin/perf-tools";
+import * as os from "os";
+
+async function sleep(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+const testCasesMap = new Map<string, any>();
+testCasesMap.set("identity transform", require("./identity-transformer"));
 
 const loggerCategory = "Transformer Performance Tests Identity";
 const assetsDir = path.join(__dirname, "assets");
 const outputDir = path.join(__dirname, ".output");
 
-describe("imodel-transformer", () => {
+const setupTestData = async () => {
+  const logLevel = +process.env.LOG_LEVEL!;
+  if (LogLevel[logLevel] !== undefined) {
+    Logger.initializeToConsole();
+    Logger.setLevelDefault(LogLevel.Error);
+    Logger.setLevel(loggerCategory, LogLevel.Info);
+    Logger.setLevel(TransformerLoggerCategory.IModelExporter, logLevel);
+    Logger.setLevel(TransformerLoggerCategory.IModelImporter, logLevel);
+    Logger.setLevel(TransformerLoggerCategory.IModelTransformer, logLevel);
+  }
+  let usrEmail;
+  let usrPass;
+  if(process.env.V2_CHECKPOINT_USER_NAME){
+    usrEmail = process.env.V2_CHECKPOINT_USER_NAME;
+    usrPass = process.env.V2_CHECKPOINT_USER_PASSWORD;
+  } else if(process.env.V1_CHECKPOINT_USER_NAME){
+    usrEmail = process.env.V1_CHECKPOINT_USER_NAME;
+    usrPass = process.env.V1_CHECKPOINT_USER_PASSWORD;
+  } else {
+    usrEmail = process.env.IMODEL_USER_NAME;
+    usrPass = process.env.IMODEL_USER_PASSWORD;
+  }
 
-  const testCases = {
-    "identity transform": require("./identity-transformer"),
+  assert(usrEmail, "user name was not configured");
+  assert(usrPass, "user password was not configured");
+  const user = {
+    email: usrEmail,
+    password: usrPass,
   };
 
+  assert(process.env.OIDC_CLIENT_ID, "");
+  assert(process.env.OIDC_REDIRECT, "");
+  const authClient = process.env.CI === "1"
+    ? new TestBrowserAuthorizationClient({
+      clientId: process.env.OIDC_CLIENT_ID,
+      redirectUri: process.env.OIDC_REDIRECT,
+      scope: "itwins:read imodels:read imodels:modify",
+      authority: "https://qa-ims.bentley.com",
+    }, user)
+    : new NodeCliAuthorizationClient({
+      clientId: process.env.OIDC_CLIENT_ID,
+      redirectUri: process.env.OIDC_REDIRECT,
+      scope: "imodelaccess:read storage:modify realitydata:read imodels:read library:read imodels:modify realitydata:modify savedviews:read storage:read library:modify itwinjs savedviews:modify",
+    });
 
-  before(async () => {
-    const logLevel = +process.env.LOG_LEVEL!;
-    if (LogLevel[logLevel] !== undefined) {
-      Logger.initializeToConsole();
-      Logger.setLevelDefault(LogLevel.Error);
-      Logger.setLevel(loggerCategory, LogLevel.Info);
-      Logger.setLevel(TransformerLoggerCategory.IModelExporter, logLevel);
-      Logger.setLevel(TransformerLoggerCategory.IModelImporter, logLevel);
-      Logger.setLevel(TransformerLoggerCategory.IModelTransformer, logLevel);
-    }
-    var usrEmail;
-    var usrPass;
-    if(process.env.V2_CHECKPOINT_USER_NAME){
-      usrEmail = process.env.V2_CHECKPOINT_USER_NAME;
-      usrPass = process.env.V2_CHECKPOINT_USER_PASSWORD;
-    }
-    else if(process.env.V1_CHECKPOINT_USER_NAME){
-      usrEmail = process.env.V1_CHECKPOINT_USER_NAME;
-      usrPass = process.env.V1_CHECKPOINT_USER_PASSWORD;
-    }
-    else {
-      usrEmail = process.env.IMODEL_USER_NAME;
-      usrPass = process.env.IMODEL_USER_PASSWORD;
-    }
+  await authClient.signIn();
 
-    assert(usrEmail, "user name was not configured");
-    assert(usrPass, "user password was not configured");
-    const user = {
-      email: usrEmail,
-      password: usrPass,
-    };
+  const hostConfig  = new IModelHostConfiguration();
+  hostConfig.authorizationClient = authClient;
+  const hubClient = new IModelsClient({ api: { baseUrl: `https://${process.env.IMJS_URL_PREFIX}api.bentley.com/imodels` } });
+  hostConfig.hubAccess = new BackendIModelsAccess(hubClient);
+  await IModelHost.startup(hostConfig);
 
-    assert(process.env.OIDC_CLIENT_ID, "");
-    assert(process.env.OIDC_REDIRECT, "");
-    const authClient = process.env.CI === '1'
-      ? new TestBrowserAuthorizationClient({
-        clientId: process.env.OIDC_CLIENT_ID,
-        redirectUri: process.env.OIDC_REDIRECT,
-        scope: "itwins:read imodels:read imodels:modify",
-        authority: "https://qa-ims.bentley.com",
-      }, user)
-      : new NodeCliAuthorizationClient({
-        clientId: process.env.OIDC_CLIENT_ID,
-        redirectUri: process.env.OIDC_REDIRECT,
-        scope: "imodelaccess:read storage:modify realitydata:read imodels:read library:read imodels:modify realitydata:modify savedviews:read storage:read library:modify itwinjs savedviews:modify",
-      });
+  const arrImodels = [];
+  for await (const iModel of getTestIModels()) {
+    arrImodels.push(iModel);
+  }
+  return arrImodels;
+};
 
-    await authClient.signIn();
-  
-    
+void (async function () {
+  const testIModels = await setupTestData();
+  const reporter = new Reporter();
 
-    const hostConfig  = new IModelHostConfiguration();
-    hostConfig.authorizationClient = authClient;
-    const hubClient = new IModelsClient({ api: { baseUrl: `https://${process.env.IMJS_URL_PREFIX}api.bentley.com/imodels` } });
-    hostConfig.hubAccess = new BackendIModelsAccess(hubClient);
-    await IModelHost.startup(hostConfig);
-  });
-
-  after(async () => {
-    //await fs.promises.rm(outputDir);
-    await IModelHost.shutdown();
-  });
-  it(`Transform Regression Tests`, async () => {
-    // const report = [] as Record<string, string | number>[];
-    const reporter = new Reporter();
-    const reportPath = initOutputFile("report.csv", outputDir);
-    var count = 0;
-    const os = require('os');
-    for await (const iModel of getTestIModels()) {
-      if((count === 2 || count === 5)){
-        describe(`Transforms of ${iModel.name}`, () => {
-        for (const [testCaseName, testCaseRun] of Object.entries(testCases)) {
-            it(testCaseName, async function () {
-            //if (!process.env.CI && iModel.tShirtsize !== 'm') this.skip();
-            await testCaseRun.default(iModel, os, reporter);
-            }).timeout(0);
-        }
+  testIModels.forEach(async (value, index, _array) => {
+    const iModel = value;
+    if (index === 2) {
+      describe(`Transforms of ${iModel.name}`, async () => {
+        testCasesMap.forEach(async (testCase, key, _map) => {
+          it(key, async () => {
+            await testCase.default(iModel, os, reporter);
+          });
         });
-      }
-      count++;
+      });
     }
-    reporter.exportCSV(reportPath);
   });
-});
+  // See 'DELAYED ROOT SUITE' on https://mochajs.org/
+  // This function is a special bcallback function provided by mocha when passing it the --delay flag. This gives us an opportunity to load in the iModels that we'll be testing so we can dynamically generate testcases.
+  run();
+})();
+
+// describe("imodel-transformer", () => {
+//   const arrImodels: any[] = [];
+//   let reporter: Reporter;
+//   let reportPath: string;
+//   const testCases = {
+//     "identity transform": require("./identity-transformer"),
+//   };
+//   const testCasesMap = new Map<string, any>();
+//   testCasesMap.set("identity transform", require("./identity-transformer"));
+
+//   before(async () => {
+
+//     reporter = new Reporter();
+//     reportPath = initOutputFile("report.csv", outputDir);
+//   });
+
+//   after(async () => {
+//     // await fs.promises.rm(outputDir);
+//     console.log("entered after");
+//     await IModelHost.shutdown();
+//     console.log("exiting after");
+//   });
+//   // const otherArray = ["testfunction", () => console.log("test2function")];
+//   // ["test", "test2"].forEach(async (value, _index, _array) => {
+//   //   describe(`Transforms of ${value}`, async () => {
+//   //     it(value, async () => {
+//   //       console.log(value);
+//   //       await sleep(2500);
+//   //     });
+//   //   });
+
+//   // });
+//   arrImodels.forEach(async (value, index, _array) => {
+//     const iModel = value;
+//     if (index === 1) {
+//       describe(`Transforms of ${iModel.name}`, async () => {
+//         testCasesMap.forEach(async (testCase, key, _map) => {
+//           it(key, async () => {
+//             await testCase.default(iModel, os, reporter);
+//           });
+//         });
+//       });
+//     }
+//   });
+
+// });
+
+// // it(`Transform Regression Tests`, async () => {
+// //   // const report = [] as Record<string, string | number>[];
+// //   const reporter = new Reporter();
+// //   const reportPath = initOutputFile("report.csv", outputDir);
+// //   var count = 0;
+// //   const os = require('os');
+
+// //     if((count === 2)){
+// //       describe(`Transforms of ${iModel.name}`, () => {
+// //       for (const [testCaseName, testCaseRun] of Object.entries(testCases)) {
+// //           it(testCaseName, async function () {
+// //           //if (!process.env.CI && iModel.tShirtsize !== 'm') this.skip();
+// //           await testCaseRun.default(iModel, os, reporter);
+// //           }).timeout(0);
+// //       }
+// //       });
+// //     }
+// //     count++;
+// //   reporter.exportCSV(reportPath);
+// // });
