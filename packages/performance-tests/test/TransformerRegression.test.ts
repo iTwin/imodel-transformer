246628de050be61e1c2cd36e25eaa3f70daacb9c
Daniel Rodriguez <DanRod1999@users.noreply.github.com>
2023-05-30 08:44:16 -0400

    test imodels 2 and 5

--- packages/performance-tests/test/TransformerRegression.test.ts
+++ packages/performance-tests/test/TransformerRegression.test.ts
@@ -105,7 +105,7 @@ void (async function () {
   // could probably add an outer describe here (surounding both foreaches) for any before after logic that isn't related to getting the test data. but idk for sure 
   testIModels.forEach(async (value, index, _array) => {
     const iModel = value;
-    if (index === 2) {
+    if (index === 2 || index === 5) {
       describe(`Transforms of ${iModel.name}`, async () => {
         testCasesMap.forEach(async (testCase, key, _map) => {
           it(key, async () => {
