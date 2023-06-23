18957c8e8d72db557d1e6518f9901c605cfa1fe6
Daniel Rodriguez <DanRod1999@users.noreply.github.com>
2023-06-23 09:26:15 -0400

    wip eslintrc

--- packages/transformer/.eslintrc.js
+++ packages/transformer/.eslintrc.js
@@ -19,6 +19,22 @@ module.exports = {
       },
     ]
   },
+  overrides: [
+    {
+      // Allow any format for object properties in backend tests, for mock data, etc
+      files: ["**/packages/test/*.test.ts"],
+      rules: {
+        "@typescript-eslint/naming-convention": [
+          "error",
+          {
+            selector: "objectLiteralProperty",
+            format: null,
+            leadingUnderscore: "allowSingleOrDouble",
+          },
+        ],
+      },
+    },
+  ],
   "parserOptions": {
     "project": "./tsconfig.json"
   }
