a5c8c7f42b1845dd40c9fad0a445d357f9227f0a
Kyrylo Volotovskyi <130470116+KyryloVolotovskyi@users.noreply.github.com>
2023-08-03 17:11:13 +0300

    Added back sourceDb copy creation to rawForkCreateFedGuids (#106)

--- packages/performance-tests/test/cases/prepare-fork.ts
+++ packages/performance-tests/test/cases/prepare-fork.ts
@@ -43,6 +43,7 @@ export default async function prepareFork(context: TestCaseContext) {
     const schemaDumpDir = fs.mkdtempSync(path.join(os.tmpdir(), "fork-test-schemas-dump-"));
     sourceDb.nativeDb.exportSchemas(schemaDumpDir);
     Logger.logInfo(loggerCategory, `dumped schemas to: ${schemaDumpDir}`);
+    throw err;
   } finally {
     addReport(
       "Prepare Fork",
