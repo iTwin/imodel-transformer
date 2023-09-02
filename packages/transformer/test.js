429057b58803354c01ecc4cdd32258721fed8c30
Michael Belousov <michael.belousov98@gmail.com>
2023-09-01 23:33:30 -0400

    kinda working maybe?

--- packages/transformer/test.js
+++ packages/transformer/test.js
@@ -27,21 +27,45 @@ async function main() {
     )
   `, (s) => assert(s.step() === DbResult.BE_SQLITE_DONE, db.nativeDb.getLastError()));
 
+  const font_remap = new Map();
+  const elem_remap = new Map();
 
-  console.log(
-    db.withSqliteStatement(`
-      SELECT RemapGeom(GeometryStream, 'font_remap', 'elem_remap')
-      FROM bis_GeometricElement3d
-      LIMIT 1
-    `, (s) => {
+  for (let i = 0; i < 100; ++i) {
+    try {
+      db.withPreparedSqliteStatement(`
+        SELECT RemapGeom(GeometryStream, 'font_remap', 'elem_remap')
+        FROM bis_GeometricElement3d
+        LIMIT 1
+      `, (s) => {
         let r;
         while ((r = s.step()) === DbResult.BE_SQLITE_ROW) {
           console.log(s.getRow());
         }
         assert(r === DbResult.BE_SQLITE_DONE, db.nativeDb.getLastError());
-      })
-  );
+      });
+      console.log("DONE!");
+      break;
+    } catch (err) {
+      const [_no, type, _remap, _found, _for, id] = err.message.split(/\s+/g);
+      const decimal = parseInt(id, 16);
+      if (type === 'element') {
+        elem_remap.set(decimal, decimal + 1);
+        db.withSqliteStatement(`
+          INSERT INTO temp.elem_remap VALUES(${decimal}, ${decimal+1})
+        `, (s) => assert(s.step() === DbResult.BE_SQLITE_DONE, db.nativeDb.getLastError()));
+      } else if (type === 'font') {
+        font_remap.set(decimal, decimal + 1);
+        db.withSqliteStatement(`
+          INSERT INTO temp.font_remap VALUES(${decimal}, ${decimal+1})
+        `, (s) => assert(s.step() === DbResult.BE_SQLITE_DONE, db.nativeDb.getLastError()));
+      } else {
+        assert("Unknown type:", type)
+      }
+    }
+  }
 
+  console.log(font_remap, elem_remap)
+  db.close();
 }
 
 void main();
