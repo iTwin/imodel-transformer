0023687b458b32cd5edc32f3790a158eaeefffeb
Nick Tessier <22119573+nick4598@users.noreply.github.com>
2024-03-20 10:51:34 -0400

    Check excludedElementIds before we decide to load the element in IModelExporter.exportElement (#164)

--- packages/transformer/src/IModelExporter.ts
+++ packages/transformer/src/IModelExporter.ts
@@ -846,6 +846,13 @@ export class IModelExporter {
       return;
     }
 
+    // Return early if the elementId is already in the excludedElementIds, that way we don't need to load the element from the db.
+    if (this._excludedElementIds.has(elementId)) {
+      Logger.logInfo(loggerCategory, `Excluded element ${elementId} by Id`);
+      this.handler.onSkipElement(elementId);
+      return;
+    }
+
     // are we processing changes?
     const isUpdate = this._sourceDbChanges?.element.insertIds.has(elementId)
       ? false
