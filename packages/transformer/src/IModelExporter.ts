fd9c1b5cf47226b9335549e38cfe168b8fbd9f7b
nick.tessier <22119573+nick4598@users.noreply.github.com>
2024-12-16 11:11:04 -0500

    add 'addCustomChangesCallback' with comment

--- packages/transformer/src/IModelExporter.ts
+++ packages/transformer/src/IModelExporter.ts
@@ -374,6 +374,16 @@ export class IModelExporter {
     );
   }
 
+  /**
+   * This function is called by the transformer as it is about to process the changesets passed to it in [[IModelTransformOptions.argsForProcessChanges]].
+   * This would be after the exporter has already processed the same set of changesets passed to the transformer in [[IModelTransformOptions.argsForProcessChanges]].
+   * This function should be used to modify the exporter's sourceDbChanges, if necessary, using [[ChangedInstanceIds.addCustomChange]]. See [[ChangedInstanceIds.addCustomChange]] for more information.
+   * @note It is guaranteed that exporter.sourceDbChanges will be defined and populated with the changesets passed to the transformer, if any, when this function is called by the transformer.
+   * @note The transformer will have built up the remap table between the source and target iModels before calling this function. This means that functions like [[IModelTransformer.context.findTargetElementId]] will return meaningful results.
+   * @note Its expected that this function be overridden by a subclass of exporter if it needs to modify sourceDbChanges.
+   */
+  public addCustomChangesCallback(): void {}
+
   /** Register the handler that will be called by IModelExporter. */
   public registerHandler(handler: IModelExportHandler): void {
     this._handler = handler;
